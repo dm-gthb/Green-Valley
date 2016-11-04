d06e714410014baff4aa8afb3b22a07f635d4583
dm-gthb <indexdm@yandex.ru>
2016-11-04 12:14:26 +0300

    tabs_js

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,33 @@
-$(document).ready(function () {
-   $('#gallery').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:7
-        },
-        1000:{
-            items:7
-        }
-    }
-})
+$(document).ready(function(){
+  $(".rest-tabs-type-1").click(function () {
+          $("#tab-family").addClass("rest-tabs__switcher-link--active");
+      $("#tab-corporate").removeClass("rest-tabs__switcher-link--active");
+  });
+  $(".rest-tabs-type-2").click(function () {
+          $("#tab-corporate").addClass("rest-tabs__switcher-link--active");
+      $("#tab-family").removeClass("rest-tabs__switcher-link--active");
 });
+});
+
+
+
+
+// $(document).ready(function () {
+//    $('#gallery').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:7
+//         },
+//         1000:{
+//             items:7
+//         }
+//     }
+// })
+// });
 
