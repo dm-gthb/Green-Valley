009c8a5a2a2fa1a8e3b2c8ac8e859411c45798a3
dm-gthb <indexdm@yandex.ru>
2016-11-05 12:08:17 +0300

    owl-dots

--- src/js/script.js
+++ src/js/script.js
@@ -12,22 +12,51 @@ $(document).ready(function(){
 
 
 
-// $(document).ready(function () {
-//    $('#gallery').owlCarousel({
-//     loop:true,
-//     margin:10,
-//     nav:true,
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         600:{
-//             items:7
-//         },
-//         1000:{
-//             items:7
-//         }
-//     }
-// })
-// });
+$(document).ready(function () {
+   $('#gallery').owlCarousel({
+    loop:true, //Зацикливаем слайдер
+        margin:10, //Отступ от картино если выводите больше 1
+        nav:false, //Отключил навигацию
+        //autoplay:true, //Автозапуск слайдера
+        smartSpeed:1000, //Время движения слайда
+        autoplayTimeout:2000, //Время смены слайда
+        responsive:{ //Адаптация в зависимости от разрешения экрана
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+});
+
+$(document).ready(function () {
+   $('#offers').owlCarousel({
+    loop:true, //Зацикливаем слайдер
+        margin:10, //Отступ от картино если выводите больше 1
+        //nav: false, //Отключил навигацию
+        //pagination: false,
+        // navText: ["<", ">"],
+        //navText: false,
+        // dots: true,
+        //autoplay:true, //Автозапуск слайдера
+        smartSpeed:1000, //Время движения слайда
+        autoplayTimeout:2000, //Время смены слайда
+        responsive:{ //Адаптация в зависимости от разрешения экрана
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+});
 
