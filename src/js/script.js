a8a386d7846c54c448d26eaa637ba1c89b22a3ac
dm-gthb <indexdm@yandex.ru>
2016-11-08 00:06:23 +0300

    form_correct

--- src/js/script.js
+++ src/js/script.js
@@ -60,5 +60,56 @@ $(document).ready(function () {
     });
 });
 
+$(document).ready(function(){
+  $(".form__place-type-1").click(function () {
+      $(".form__place-types-item--hotel").addClass("form__place-types-item--active");
+      $(".form__place-types-item--cottage").removeClass("form__place-types-item--active");
+  });
+  $(".form__place-type-2").click(function () {
+      $(".form__place-types-item--cottage").addClass("form__place-types-item--active");
+      $(".form__place-types-item--hotel").removeClass("form__place-types-item--active");
+});
+});
+
+
+$(document).ready(function(){
+  $(".form__payment-type-1").click(function () {
+
+      $(".form__payment-on-place-title").addClass("form__payment-on-place-title--active");
+      $(".form__payment-now-title").removeClass("form__payment-now-title--active");
+  });
+  $(".form__payment-type-2").click(function () {
+      $(".form__payment-now-title").addClass("form__payment-now-title--active");
+      $(".form__payment-on-place-title").removeClass("form__payment-on-place-title--active");
+});
+});
+
+$(document).ready(function(){
+  $("#form__hotel-type-1").click(function () {
+
+      $("label[for='form__hotel-type-1']").addClass("form__hotel-types-item--active");
+      $("label[for='form__hotel-type-2']").removeClass("form__hotel-types-item--active");
+      $("label[for='form__hotel-type-3']").removeClass("form__hotel-types-item--active");
+      $("label[for='form__hotel-type-4']").removeClass("form__hotel-types-item--active");
+  });
+  $("#form__hotel-type-2").click(function () {
+      $("label[for='form__hotel-type-2']").addClass("form__hotel-types-item--active");
+      $("label[for='form__hotel-type-1']").removeClass("form__hotel-types-item--active");
+      $("label[for='form__hotel-type-3']").removeClass("form__hotel-types-item--active");
+      $("label[for='form__hotel-type-4']").removeClass("form__hotel-types-item--active");
+});
+   $("#form__hotel-type-3").click(function () {
+      $("label[for='form__hotel-type-3']").addClass("form__hotel-types-item--active");
+      $("label[for='form__hotel-type-1']").removeClass("form__hotel-types-item--active");
+      $("label[for='form__hotel-type-2']").removeClass("form__hotel-types-item--active");
+      $("label[for='form__hotel-type-4']").removeClass("form__hotel-types-item--active");
+});
+    $("#form__hotel-type-4").click(function () {
+      $("label[for='form__hotel-type-4']").addClass("form__hotel-types-item--active");
+      $("label[for='form__hotel-type-1']").removeClass("form__hotel-types-item--active");
+      $("label[for='form__hotel-type-2']").removeClass("form__hotel-types-item--active");
+      $("label[for='form__hotel-type-3']").removeClass("form__hotel-types-item--active");
+});
+});
 
 
